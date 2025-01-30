aac0e4475b69da7fe63118cd84482a45ec844274
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-30 23:59:50 +0530

    user router created

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const app = express();
 const { run } = require("./config/db");
 const port = process.env.PORT || 4000;
 require("dotenv").config();
+const userRouter = require('./routes/userRoutes')
+
 // middlewair
 
 app.use(cors());
 app.use(express.json());
 run();
+
+
+
+
+
+app.get('/api' , userRouter )
+
+
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send({
     status: true,
---
+++ routes/userRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router()
+const {client} = require('../config/db')
+
+
+module.exports = router;
