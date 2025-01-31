ba61f201d83182dcb4a8a2f2f7f3f278b33a90ab
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-31 14:06:03 +0530

    categories api and popular-oproudcts created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,25 @@ const { client } = require("../config/db");
 module.exports = router;
 const db = client.db("Green-basket");
 const categoryCollection = db.collection("category");
+const productCollection = db.collection("products");
 
 router.get("/categories", async (req, res) => {
   try {
-      const categories = await categoryCollection.find().toArray()
-      res.send(categories)
-  } catch {}
+    const categories = await categoryCollection.find().toArray();
+    res.send(categories);
+  } catch {
+    res.send({
+      massage: "404",
+    });
+  }
+});
+router.get("/popular-products", async (req, res) => {
+  try {
+    const products = await productCollection.find().toArray();
+    res.send(products);
+  } catch {
+    res.send({
+      massage: "404",
+    });
+  }
 });
